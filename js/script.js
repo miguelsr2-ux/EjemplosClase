8c752b535cbe4c75f6b9642fcfe4e75d397f9724
Miguel <miguel_sanr3@comunidad.unam.mx>
2025-11-11 15:31:44 -0600

    init: Creando repo

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//const x=10;
+//x=1000;
+//console.log(x);
+
+//iteraciones
+const cielo = document.getElementById("cielo");
+
+for(let i=0; i<5; i++){
+    cielo.innerHTML += '⭐';
+}
+
+const planetas = ["🌍", "🪐", "🌕"]
+
+for(const planeta of planetas){
+    cielo.innerHTML += planeta;
+}
+
+let n = 0
+
+while(n<5){
+    cielo.innerHTML += "☁️";
+    n++;
+}
+
+let m = 0;
+do{
+    cielo.innerHTML += "🚀";
+    m++;
+}while(m<4)
+
+const btnEstrella = document.getElementById("btnEstrella");
+
+btnEstrella.addEventListener("click", ()=> {
+    cielo.innerHTML += "⭐";
+})
